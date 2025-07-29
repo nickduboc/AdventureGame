77187c7403e6bb04affc8fe00686eadf2011a05c
Nicholas <nick_cduboc@hotmail.com>
2025-07-29 15:22:34 -0400

    weapon, monster, hp

--- src/adventureGame.js
+++ src/adventureGame.js
@@ -19,4 +19,15 @@ let inventory = [];
 //Ask name
 playerName = readline.question("\nWhat is your name? ");
 console.log("\nHello, " + playerName + "!");
-console.log("You have " + gold + " gold");
+console.log("\nYou have " + gold + " gold");
+//Weapon
+let weaponDamage = 0;
+console.log("\nYour weapon damage is "+ weaponDamage);
+console.log("When you buy a sword, weapon damage will increase to 10!");
+//Monster Defense value - higher when late game
+let monsterDef = 5;
+console.log("\nMonster Defense is " + monsterDef);
+console.log("Monsters can withstand some damage in combat!");
+//Health Potion
+let healingPotionValue = 30;
+console.log("\nThe Healing Potion can restore up to " + healingPotionValue + " HP!");
